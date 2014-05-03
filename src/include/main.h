5bb68e7dc3f37ad4325c49d5ed0e025a25e02240
Damian Galvez <damian.galvez@globant.com>
2014-05-03 17:55:33 -0300

    add know_client struc

--- src/include/main.h
+++ src/include/main.h
@@ -3,22 +3,28 @@
 
 #define PIPE_SIZE 1024 // tamanio para leer/escribir en el PIPE
 #define CLIENT_PORT 4444
-#define CONFIG_FILE_PATH "/home/tincho/config.json"
+#define CONFIG_FILE_PATH ""
 
 #define SERVER_STATUS_ACTIVE 0
 #define SERVER_STATUS_INACTIVE 1
+
+typedef struct known_client_struct Known_client;
+struct known_client_struct {
+	char* ip;
+	int active;
+	int port;
+};
+
 
 typedef struct server_struct Server;
 struct server_struct {
 	char *name;
 	int server_fd;
 	char* server_port;
-	char* known_clients[3];
-	int actives_clients[3];
+	Known_client known_clients[3];
 	int status;
 	char *files;
 };
-
 
 
 /*typedef struct config_struct Config;
